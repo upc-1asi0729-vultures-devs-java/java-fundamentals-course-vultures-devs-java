e641fd50e356a3eea95a2997b98d69d17adeeec7
Cmarin2802 <cesarmarin2802@gmail.com>
2026-06-20 20:21:36 -0500

    feat: update lesson 02 starter files

--- starter-files/lesson-02/Main.java
+++ starter-files/lesson-02/Main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Aventura de Bloques ===");
+        System.out.println("Bienvenido, explorador.");
+        System.out.println("En este curso aprenderas Java creando una aventura de bloques.");
+        System.out.println("Tu mision sera minar, explorar y guardar objetos en un inventario.");
+    }
+}
